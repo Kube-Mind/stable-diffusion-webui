be5729c302a99aadb374c8a109cb8ca3f62dc748
jesmigel <jesmigel.developer@gmail.com>
2025-09-01 07:25:43 +1000

    ci: fix argowf delegation script params

--- tools/scripts/trigger-build.sh
+++ tools/scripts/trigger-build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+GIT_URL=https://github.com/Kube-Mind/stable-diffusion-webui.git
+OCI_IMAGE_NAME=comfyui
+ARGOWF_BEARER_TOKEN=$1
 
 # Load submodule info from JSON
 COMFYUI_TAG=$(jq -r '.comfyui.tag' submodules.json)
-COMFYUI_MANAGER_TAG=$(jq -r '.comfyui-manager.tag' submodules.json)
 
 # Example: choose one submodule to drive OCI_IMAGE_TAG
 export OCI_IMAGE_TAG="$COMFYUI_TAG"
@@ -10,11 +12,11 @@ export OCI_IMAGE_TAG="$COMFYUI_TAG"
 # Trigger build with dynamic JSON
 curl 'https://wf.jcan.dev/wf/webhooks/buildkit' \
   -H 'Content-Type: application/json' \
-  -H "Authorization: ${{ secrets.ARGOWF_BEARER_TOKEN }}" \
+  -H "Authorization: $ARGOWF_BEARER_TOKEN" \
   -d "{
-      \"repo\": \"${{ github.server_url }}/${{ github.repository }}.git\",
+      \"repo\": \"$GIT_URL.git\",
       \"branch\": \"main\",
-      \"dockerfile\": \"images/${{ env.OCI_IMAGE_NAME }}\",
-      \"image\": \"ocr.jcan.dev/library/sd-${{ env.OCI_IMAGE_NAME }}:$OCI_IMAGE_TAG\",
+      \"dockerfile\": \"images/$OCI_IMAGE_NAME\",
+      \"image\": \"ocr.jcan.dev/library/sd-$OCI_IMAGE_NAME:$OCI_IMAGE_TAG\",
       \"context\": \"./\"
     }"
